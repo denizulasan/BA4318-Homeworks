95b38532e6946aaa2db204ebb764f5f7128ffc6a
denizulasan <48112748+denizulasan@users.noreply.github.com>
2019-03-01 11:24:09 +0300

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+a = 5
+b = 8
+c = 4
+x = a + b + c
+print( "x is" , x)
+numberofintegers=3
+average= x/ numberofintegers
+print( "average is" , average)
+average = int (5.66)
+print("average from 5.66 is", average)
+
